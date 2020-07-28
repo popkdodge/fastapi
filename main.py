c0e55dd824fbb74cd0c9a81bfe87d391dd07ee93
popkdodge <popkdodge@gmail.com>
2020-07-28 14:33:44 -0400

    pleasework

--- main.py
+++ main.py
@@ -51,3 +51,29 @@ def predict(pred: Pred):
     response_dict = dict(zip(price_list, response_list))
     response_json = json.dumps(response_dict)
     return f'{str(response_json)}'
+
+
+@app.post('/predict2')
+def predict2(pred: Pred):
+    import pandas as pd
+    import numpy as np
+    import joblib
+    import pickle
+    import tensorflow as tf
+
+    test_model_data = pd.read_csv('test_model.csv')
+#        test_model_data.room_type = 1.0
+#    if pred.roomtype == 'Entire home/apt':
+#    elif pred.roomtype == 'Private room':
+#        test_model_data.room_type = 2.0
+#    else:
+#        test_model_data.room_type = 3.0
+# j    test_model_data.zipcode = pred.zipcode
+    model = tf.keras.models.load_model('kristine_model_0')
+    response = round(model.predict(test_model_data)[[0]][0][0], 2)
+    response = np.expm1(response)
+    price_list = ['Prices']
+    response_list = [str(round(response, 2))]
+    response_dict = dict(zip(price_list, response_list))
+    response_json = json.dumps(response_dict)
+    return f'{str(response_json)}'
