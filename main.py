67513e9b2f9377898630272052ea6961c9491e9d
popkdodge <popkdodge@gmail.com>
2020-07-28 17:16:01 -0400

    Please Work

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import uvicorn
 from typing import Dict
 from pydantic import BaseModel
 import json
-
+import pandas as pd
+import numpy as np
+import joblib
+import pickle
+import tensorflow as tf
+from geopy.geocoders import Nominatim
 
 app = FastAPI()
 
@@ -32,10 +37,7 @@ def home():
 
 @app.post('/predict')
 def predict(pred: Pred):
-    import pandas as pd
-    import numpy as np
-    import joblib
-    import pickle
+    """This wil use Unit 3 model!"""
     test_model_data = pd.read_csv('models/sample_pred.csv')
     test_model_data.room_type = pred.roomtype
     test_model_data.accomodates = pred.accomodates
@@ -55,20 +57,42 @@ def predict(pred: Pred):
 
 @app.post('/predict2')
 def predict2(pred: Pred):
-    import pandas as pd
-    import numpy as np
-    import joblib
-    import pickle
-    import tensorflow as tf
+    '''This will use Unit 4 model!'''
 
+    # This will call the test rows
     test_model_data = pd.read_csv('test_model.csv')
-#        test_model_data.room_type = 1.0
-#    if pred.roomtype == 'Entire home/apt':
-#    elif pred.roomtype == 'Private room':
-#        test_model_data.room_type = 2.0
-#    else:
-#        test_model_data.room_type = 3.0
-# j    test_model_data.zipcode = pred.zipcode
+    # Editing test rows
+    if pred.roomtype == 'Entire home/apt':
+        test_model_data.room_type = 1.0
+    elif pred.roomtype == 'Private room':
+        test_model_data.room_type = 2.0
+    else:
+        test_model_data.room_type = 3.0
+
+    # Applying
+    test_model_data.accomodates = pred.accomodates
+    test_model_data.bathrooms = pred.bathrooms
+    test_model_data.city = pred.city
+    test_model_data.latitude = pred.latitude
+    test_model_data.longitude = pred.longitude
+    test_model_data.zipcode = 0.0
+    test_model_data.review_scores_rating = pred.reviewscoresrating
+    test_model_data.bedrooms = pred.bedrooms
+    test_model_data.TV = pred.tv
+
+    # This will geocode
+    ''' This will geocode addresses. And will set a dummy data if addrest do not work! '''
+    geolocator = Nominatim(user_agent="Test")
+
+    try:
+        location = geolocator.geocode(f"{pred.streetaddress} , {pred.zipcode}")
+        test_model_data.zipcode = 0.0
+        test_model_data.latitude = location.latitude
+        test_model_data.longitude = location.longitude
+    except:
+        pass
+
+    # This is where the model is used.
     model = tf.keras.models.load_model('kristine_model_0')
     response = round(model.predict(test_model_data)[[0]][0][0], 2)
     response = np.expm1(response)
@@ -76,4 +100,5 @@ def predict2(pred: Pred):
     response_list = [str(round(response, 2))]
     response_dict = dict(zip(price_list, response_list))
     response_json = json.dumps(response_dict)
+
     return f'{str(response_json)}'
